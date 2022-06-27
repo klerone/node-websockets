cf548d9dbec83c8a09356651e8ea184640f8098a
klerone <klerone@users.noreply.github.com>
2022-06-27 10:56:32 +0800

    add message

--- server.js
+++ server.js
@@ -14,11 +14,13 @@ const wss = new Server({ server });
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
+  client.sent("Bienvenido cerote");
   ws.on('close', () => console.log('Client disconnected'));
 });
 
 setInterval(() => {
   wss.clients.forEach((client) => {
     client.send(new Date().toTimeString());
+	client.sent("Hola cerote");
   });
 }, 1000);
